96ac00a2859b24df8dd8c8ee55eb5ca51767c82e
Harold Truong <harold.truong@gmail.com>
2020-09-24 03:09:28 -0600

    Incorporated collision between bird and pipes.

--- src/birdPipes.cpp
+++ src/birdPipes.cpp
@@ -16,7 +16,7 @@ int main()
     
     myBird bird(350.f, 180.f);
     
-    bool collision = true;
+    bool collision = false;
     bool pause = false;
     sf::CircleShape circle(10.f);
     myPipes[0].create();
@@ -60,11 +60,19 @@ int main()
 
 // set the shape color to blue
 circle.setFillColor(sf::Color(255,0,0));
-//        if (collision){
-//            rectangle1top.setFillColor(sf::Color(255,255,0));
-//        } else {
-//            rectangle1top.setFillColor(sf::Color(70,130,180));
-//        }
+        if (collision){
+            myPipes[1].recTOP.setFillColor(sf::Color(255,255,0));
+            myPipes[1].recBOT.setFillColor(sf::Color(255,255,0));
+        } else {
+             myPipes[0].recTOP.setFillColor(sf::Color(70,130,180));
+             myPipes[0].recBOT.setFillColor(sf::Color(70,130,180));
+            myPipes[1].recTOP.setFillColor(sf::Color(250,0,0));
+            myPipes[1].recBOT.setFillColor(sf::Color(250,0,0));
+            myPipes[2].recTOP.setFillColor(sf::Color(50,205,50));
+            myPipes[2].recBOT.setFillColor(sf::Color(50,205,50));
+            myPipes[3].recTOP.setFillColor(sf::Color(153,50,204));
+            myPipes[3].recBOT.setFillColor(sf::Color(153,50,204));
+        }
 //circle.setOutlineThickness(10.f);
         circle.setPosition(390.f, bird.posY);
         myPipes[0].position();
@@ -80,11 +88,12 @@ circle.setFillColor(sf::Color(255,0,0));
     // end the current frame
         window.display();
     
-//        if(circle.getGlobalBounds().intersects(rectangle1top.getGlobalBounds())){
-//                collision = true;
-//            } else {
-//                collision = false;
-//            }
+        if(circle.getGlobalBounds().intersects(myPipes[1].recTOP.getGlobalBounds()) || circle.getGlobalBounds().intersects(myPipes[1].recBOT.getGlobalBounds())){
+                collision = true;
+            } else {
+                collision = false;
+            }
+        
         if(!pause){
             bird.moveY(-0.0008); //Update circle position
             myPipes[0].move(-.2);
@@ -97,12 +106,6 @@ circle.setFillColor(sf::Color(255,0,0));
             myPipes.push_back(myPipe(myPipes[3].posX+300.f));
             myPipes[4].create();
             myPipes.erase(myPipes.begin());
-            int counter = 0;
-            for(myPipe x: myPipes)
-            {
-                counter +=1;
-            }
-            cout << counter << endl;
             
            // myPipes[0] = myPipe(myPipes[3].posX+300.f);
            //myPipes[0].create();
